13f627d324338ccfd1c1179d5a1ffc295056413b
Itay Barr <itaybarbar@gmail.com>
2022-04-16 12:11:04 +0300

    updated app to React 18

---
+++ build/precache-manifest.cb98b749d34e18f9b8453175216f5f45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5d1bdaff52b8cdf511124960fde5ab0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b4ae4f679bdf8a07ba8",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "6ff2653192b1504f3a7d",
+    "url": "/static/css/main.f6d65909.chunk.css"
+  },
+  {
+    "revision": "2b4ae4f679bdf8a07ba8",
+    "url": "/static/js/2.f7eb401d.chunk.js"
+  },
+  {
+    "revision": "6ff2653192b1504f3a7d",
+    "url": "/static/js/main.3605d190.chunk.js"
+  },
+  {
+    "revision": "3077d470ae89939591ef",
+    "url": "/static/js/runtime~main.83bf741c.js"
+  }
+]);
